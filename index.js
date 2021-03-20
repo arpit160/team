1f176c0b921504d0fc7ea37c5dadee867036193d
arpit160 <singhalarpit160@gmail.com>
2021-03-20 21:59:23 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+express=require('express')
+app=express();
+path=require('path')
+
+
+engine = require('ejs-mate')
+app.engine('ejs', engine);
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/',(req,res)=>
+{
+    res.render("home")
+})
+
+
+app.get('/login',(req,res)=>
+{
+    res.render("login.ejs");
+})
+
+app.get('/register',(req,res)=>
+{
+    res.render("register.ejs");
+})
+
+app.get('/blogpost',(req,res)=>
+{
+    res.render("blogpost.ejs");
+})
+
+let port=process.env.port || 3000;
+
+
+app.listen(port,()=>
+{
+    console.log("listening on port 3000")
+})
